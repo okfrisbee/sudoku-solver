676e124900cb1652b9dfd0633d4720e85bad7c82
Fristine Sok <166497861+okfrisbee@users.noreply.github.com>
2025-08-31 01:14:48 -0700

    Add benchmarking function

--- main.py
+++ main.py
@@ -7,29 +7,31 @@ def get_sudoku_puzzles(file):
         for line in f:
             digits.append(line.split()[1])
         return digits
-        
-def main():
-    board = "800000000043900008001000405039600027000000000006230004000300040020800900000005102"
-    startTime = time.perf_counter()
-    mrv = Sudoku.Sudoku(board)
-    mrv.print_board()
-    mrv.backtrack_with_mrv_fc()
-    mrv.print_board()
-    endTime = time.perf_counter()
-    mrv_time = endTime - startTime
-
-    startTime = time.perf_counter()
-    no_mrv = Sudoku.Sudoku(board)
-    no_mrv.backtrack()
-    no_mrv.print_board()
-    endTime = time.perf_counter()
-    no_mrv_time = endTime - startTime
     
-    print()
-    print(f"Total Time Took:\nMRV: {mrv_time}\nNo MRV: {no_mrv_time}")
+def benchmark():
+    no_mrv_time = 0
+    mrv_time = 0
+    for i, puzzle in enumerate(get_sudoku_puzzles("puzzle_bank.txt")):
+        startTime = time.perf_counter()
+        no_mrv = Sudoku.Sudoku(puzzle)
+        no_mrv.backtrack()
+        endTime = time.perf_counter()
+        no_mrv_time += endTime - startTime
+        print(f"No MRV Finished {i + 1} and {puzzle}")
+
+        startTime = time.perf_counter()
+        mrv = Sudoku.Sudoku(puzzle)
+        mrv.backtrack_with_mrv_fc()
+        endTime = time.perf_counter()
+        mrv_time += endTime - startTime
+        print(f"MRV Finished {i + 1} and {puzzle}")
+
+    print(f"Total Time Took for 200 Puzzles:\nNo MRV: {no_mrv_time}\nMRV: {mrv_time}")
     quickest = "MRV" if mrv_time < no_mrv_time else "No MRV"
     print(f"{quickest} was faster by {round(max(mrv_time, no_mrv_time)/min(mrv_time, no_mrv_time))} times.")
         
+def main():
+    benchmark()
 
 if __name__ == "__main__":
     main()
